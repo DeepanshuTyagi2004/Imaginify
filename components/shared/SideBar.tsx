2e3ced8a9737ce54d16a802d49472a85bb542e52
Deepanshu <admin@Admins-MacBook-Pro.local>
2024-06-18 23:10:53 +0530

    sidebar completed for All users

--- components/shared/SideBar.tsx
+++ components/shared/SideBar.tsx
@@ -3,9 +3,10 @@
 import Link from 'next/link'
 import Image from 'next/image'
 import React from 'react'
-import { SignedIn, SignedOut } from '@clerk/nextjs'
+import { SignedIn, SignedOut, UserButton } from '@clerk/nextjs'
 import { navLinks } from '@/constants'
 import { usePathname } from 'next/navigation'
+import { Button } from '../ui/button'
 
 function SideBar() {
   const pathname = usePathname();
@@ -23,7 +24,7 @@ function SideBar() {
         <nav className='sidebar-nav'>
           <SignedIn>
             <ul className='sidebar-nav_elements'>
-              {navLinks.map((link) => {
+              {navLinks.slice(0, 6).map((link) => {
                 const isActive = link.route === pathname
                 return (
                   <li key={link.route} className={`sidebar-nav_element group ${isActive ? 'bg-purple-gradient text-white' : 'text-grey-700'}`}>
@@ -42,9 +43,39 @@ function SideBar() {
                 )
               })}
             </ul>
+            <ul className='sidebar-nav_elements'>
+              {navLinks.slice(6).map((link) => {
+                const isActive = link.route === pathname
+                return (
+                  <li key={link.route} className={`sidebar-nav_element group ${isActive ? 'bg-purple-gradient text-white' : 'text-grey-700'}`}>
+                    <Link className='sidebar-link' href={link.route}>
+                      <Image
+                        src={link.icon}
+                        alt='logo'
+                        width={24}
+                        height={24}
+                        className={`${isActive && 'brightness-200'}`}
+                      />
+                      {link.label}
+
+                    </Link>
+                  </li>
+                )
+              })}
+              <li className='flex-center cursor-pointer gap-2 p-4'>
+                <UserButton afterSignOutUrl='/' showName />
+              </li>
+            </ul>
           </SignedIn>
+
           <SignedOut>
-            
+            <Button asChild className='button bg-purple-gradient bg-cover'>
+              <Link
+                href="/sign-in"
+              >
+                Login
+              </Link>
+            </Button>
           </SignedOut>
         </nav>
       </div>
