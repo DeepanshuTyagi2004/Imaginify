414aa745fd15bb37516ac67c1be2107e3bd8cf94
Deepanshu <admin@Admins-MacBook-Pro.local>
2024-06-18 22:48:44 +0530

    Sidebar completed for SignedIn users

--- components/shared/SideBar.tsx
+++ components/shared/SideBar.tsx
@@ -1,8 +1,14 @@
+'use client'
+
 import Link from 'next/link'
 import Image from 'next/image'
 import React from 'react'
+import { SignedIn, SignedOut } from '@clerk/nextjs'
+import { navLinks } from '@/constants'
+import { usePathname } from 'next/navigation'
 
 function SideBar() {
+  const pathname = usePathname();
   return (
     <aside className='sidebar'>
       <div className='flex flex-col size-full gap-4'>
@@ -14,6 +20,33 @@ function SideBar() {
             height={28}
           />
         </Link>
+        <nav className='sidebar-nav'>
+          <SignedIn>
+            <ul className='sidebar-nav_elements'>
+              {navLinks.map((link) => {
+                const isActive = link.route === pathname
+                return (
+                  <li key={link.route} className={`sidebar-nav_element group ${isActive ? 'bg-purple-gradient text-white' : 'text-grey-700'}`}>
+                    <Link className='sidebar-link' href={link.route}>
+                      <Image
+                        src={link.icon}
+                        alt='logo'
+                        width={24}
+                        height={24}
+                        className={`${isActive && 'brightness-200'}`}
+                      />
+                      {link.label}
+
+                    </Link>
+                  </li>
+                )
+              })}
+            </ul>
+          </SignedIn>
+          <SignedOut>
+            
+          </SignedOut>
+        </nav>
       </div>
     </aside>
   )
