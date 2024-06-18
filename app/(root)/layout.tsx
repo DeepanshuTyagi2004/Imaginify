97ad03cacba4cf3629a8946045df5c101a232c78
Deepanshu <admin@Admins-MacBook-Pro.local>
2024-06-19 00:03:31 +0530

    mobile nav completed

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import SideBar from '../../components/shared/SideBar'
+import MobileNav from '@/components/shared/MobileNav'
 
 function layout({children} : {children: React.ReactNode}) {
   return (
     <main className='root'>
       <SideBar />
+      <MobileNav />
       <div className='root-container'>
         <div className='wrapper'>
           {children}
