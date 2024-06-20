ae512a6b818c2740160a31e82d15a62906054114
Deepanshu <admin@Admins-MacBook-Pro.local>
2024-06-20 22:48:32 +0530

    Server actions created

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,15 +1,12 @@
 import React from 'react'
-import {UserButton} from '@clerk/nextjs'
+import { UserButton } from '@clerk/nextjs'
 
 function Home() {
-  return (  
+  return (
     <div>
-      <p>
-        Home
-      </p>
-      <UserButton afterSignOutUrl='/' />
+
     </div>
-  ) 
+  )
 }
 
 export default Home 
